1af51932d416195c33ed71edaf5bd10c638f3c89
Joselo <ordonezjoselo531@gmail.com>
2023-12-19 10:51:43 -0500

    Cambios whitenoise

--- Rehabilitation/settings.py
+++ Rehabilitation/settings.py
@@ -146,7 +146,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('signin')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
@@ -157,7 +157,6 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
 
 
     
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
