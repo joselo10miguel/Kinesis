8230bce13074f69c09b0f8d85272c8c531d0cd3f
Joselo <ordonezjoselo531@gmail.com>
2023-12-21 12:47:24 -0500

    a ver si ya ahora si

--- Rehabilitation/settings.py
+++ Rehabilitation/settings.py
@@ -146,7 +146,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('signin')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
