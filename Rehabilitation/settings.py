5251aca576909eb3d16685a34b9b50a4f4636552
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-12-30 15:18:26 -0500

    pequeño ajuste

--- Rehabilitation/settings.py
+++ Rehabilitation/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-from django.urls import reverse_lazy
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
