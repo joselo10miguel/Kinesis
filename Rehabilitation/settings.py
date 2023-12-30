2380a2404ce06ec597062046239055144456f8e9
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-12-30 15:32:13 -0500

    commit de prueba

--- Rehabilitation/settings.py
+++ Rehabilitation/settings.py
@@ -142,9 +142,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+# LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 
-LOGOUT_REDIRECT_URL = reverse_lazy('signin')
+# LOGOUT_REDIRECT_URL = reverse_lazy('signin')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
