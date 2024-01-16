d5aebfe9cacf5aca1c5627fb704567da966faa5b
Joselo <ordonezjoselo531@gmail.com>
2024-01-16 08:58:42 -0500

    configurar https wss 2

--- routing.py
+++ routing.py
@@ -3,20 +3,21 @@ from django.urls import path
 from django.core.asgi import get_asgi_application
 from  AppRehabilitation.views  import RutinaLuxacionConsumer, RutinaLesionMediaConsumer
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
-    "http": get_asgi_application(),
-    "https": get_asgi_application(),
+    "http": get_asgi_application,
+    "https": get_asgi_application,
 
     # WebSocket chat handler
-    "websocket":
+    "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
                  path('wss/rutina/luxacion/', RutinaLuxacionConsumer.as_asgi()),
                  path('wss/rutina/lesion/media/', RutinaLesionMediaConsumer.as_asgi()),
             ])
         )
-    
+    ),
 })
