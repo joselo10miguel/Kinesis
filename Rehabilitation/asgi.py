b056483acce22c7d9cea4c786bea8b91a9499c68
AlexVj12 <85960309+AlexVj12@users.noreply.github.com>
2024-01-17 13:16:58 -0500

    Update asgi.py

--- Rehabilitation/asgi.py
+++ Rehabilitation/asgi.py
@@ -1,16 +1,29 @@
 """
-ASGI config for Rehabilitation project.
+ASGI config for Test project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
 import os
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test.settings')
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rehabilitation.settings')
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "https": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
