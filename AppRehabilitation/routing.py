9541766955693d95d589e8342e32d2f531e57892
AlexVj12 <85960309+AlexVj12@users.noreply.github.com>
2024-01-17 13:21:46 -0500

    Update routing.py

--- AppRehabilitation/routing.py
+++ AppRehabilitation/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 
 websocket_urlpatterns = [
     re_path(r"rutina/luxacion/", RutinaLuxacionConsumer.as_asgi()),
-     re_path(r"rutina/lesion/media/", RutinaLesionMediaConsumer.as_asgi()),
+    re_path(r"rutina/lesion/media/", RutinaLesionMediaConsumer.as_asgi()),
 ]
