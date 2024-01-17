216135fb8c08199230fd706201b0b4f305950076
AlexVj12 <85960309+AlexVj12@users.noreply.github.com>
2024-01-17 13:21:32 -0500

    Update asgi.py

--- Rehabilitation/asgi.py
+++ Rehabilitation/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from chat.routing import websocket_urlpatterns
+from AppRehabilitation.routing import websocket_urlpatterns
 
 django_asgi_app = get_asgi_application()
 
