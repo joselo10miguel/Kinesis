36eda421f1c37d46cf2edb9d4b034f83d609639f
AlexVj12 <85960309+AlexVj12@users.noreply.github.com>
2024-01-17 13:08:19 -0500

    Update asgi.py

--- Rehabilitation/asgi.py
+++ Rehabilitation/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rehabilitation.settings')
 
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
@@ -15,7 +16,6 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from AppRehabilitation.routing import websocket_urlpatterns
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rehabilitation.settings')
 
 django_asgi_app = get_asgi_application()
 
